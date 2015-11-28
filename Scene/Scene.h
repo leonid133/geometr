dc42042eabb5322be8911799bda1dba3b27b0473
ln.blohin <ln.blohin@tensor.ru>
2015-11-28 23:35:50 +0300

    scene

---
+++ Scene/Scene.h
@@ -0,0 +1,33 @@
+#ifdef SCENE_EXPORTS
+#define SCENE_API __declspec(dllexport)
+#else
+#define SCENE_API __declspec(dllimport)
+#endif
+
+namespace Scene
+{
+    class SCENE_API MyScene
+    {
+    private:
+        
+    public:
+        MyScene(){};
+        int LoadMyShape( const std::string &namedll )
+        {
+            HINSTANCE h;
+            h=LoadLibrary( namedll );
+            if (!h)
+            {
+                printf("Ошибка - не могу найти Geometr.dll\n");
+                return 1;
+            }
+            Geometr::MyShape *pVar;
+            (FARPROC &)pVar = GetProcAddress(hLib, "fig");
+            //*pVar = 123;
+
+            FreeLibrary(h);
+            return 0;
+        };
+        ~MyScene(){};
+    };
+}
